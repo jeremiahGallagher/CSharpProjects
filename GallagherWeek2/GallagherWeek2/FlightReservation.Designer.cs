800ca607fdb267dc025a5323e327aacb89e30ad2
Jeremiah <miahg17@gmail.com>
2018-06-04 00:05:04 -0500

    Add files via upload

---
+++ GallagherWeek2/GallagherWeek2/FlightReservation.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace GallagherWeek2
+{
+    partial class FlightReservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(662, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please Select First Class or Economy For Your Seating Assignment";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "First Class";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(463, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Economy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seats 1-5";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Seats 6-10";
+            // 
+            // FlightReservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 281);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FlightReservation";
+            this.ShowIcon = false;
+            this.Text = "Flight Seating Assignment";
+            this.Load += new System.EventHandler(this.FlightReservation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GallagherWeek2/GallagherWeek2/FlightReservation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GallagherWeek2
+{
+    public partial class FlightReservation : Form
+    {
+        public FlightReservation()
+        {
+            InitializeComponent();
+        }
+        //Constant to hold max number of seats
+        const int MaxSeats = 10;
+
+        //Create and initialize array of bools for tracking the seats
+        bool[] isAssignedSeat = new bool[MaxSeats];
+
+
+        private void FlightReservation_Load(object sender, EventArgs e)
+        {
+
+        }
+        /* When the button labeled First Class is clicked, the bool array isAssignedSeat indexes 
+         * 0-4 are checked to see if they are false, if false, they are turned to true and then a 
+         * messageBox is presented to inform the customer of their seat assignment.  This will 
+         * assign the seats in order from least to greatest. For this reason, if the largest index
+         * is already assigned true then the user will receive the Message that all First Class
+         * Seats have been assigned */ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i <= 4; i++)
+            {
+                int j = i + 1;
+                if (!isAssignedSeat[i])
+                {
+                    isAssignedSeat[i] = true;
+                    MessageBox.Show("You Have Been Assigned Seat # " +  j);
+                    break;
+                }
+                if (isAssignedSeat[4])
+                {
+                    MessageBox.Show("All First Class Seats Are Taken. Please Select an Economy Class Seat.  If you do not Want economy or one is not available, The next Flight Will Depart in 3 Hours");
+                    break;
+                }
+                
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /* When the button labeled Economy is clicked, the bool array isAssignedSeat indexes 
+         * 5-9 are checked to see if they are false, if false, they are turned to true and then a 
+         * messageBox is presented to inform the customer of their seat assignment.  This will 
+         * assign the seats in order from least to greatest. For this reason, if the largest index
+         * is already assigned true then the user will receive the Message that all Economy
+         * Seats have been assigned */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 5; i <= 9; i++)
+            {
+                int j = i + 1;
+                if (!isAssignedSeat[i])
+                {
+                    isAssignedSeat[i] = true;
+                    MessageBox.Show("You Have Been Assigned Seat # " + j);
+                    break;
+                }
+                if (isAssignedSeat[9])
+                {
+                    MessageBox.Show("All Economy Seats Are Taken. Please Select a First Class Seat.  If you do not Want First Class or one is not available, The next Flight Will Depart in 3 Hours");
+                    break;
+                }
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
