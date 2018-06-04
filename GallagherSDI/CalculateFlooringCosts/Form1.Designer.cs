fc37aff0afb8d73bfb494c52144eb1abe73847f5
Jeremiah <miahg17@gmail.com>
2018-06-03 23:54:00 -0500

    Add files via upload

---
+++ GallagherSDI/CalculateFlooringCosts/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CalculateFlooringCosts
+{
+    partial class frmFlooringCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newCarpetingCostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newHardwoodCostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newCarpetingCostToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newHardwoodCostToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileVerticallyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileHorizontallyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newCarpetingCostToolStripMenuItem,
+            this.newHardwoodCostToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1210, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newCarpetingCostToolStripMenuItem
+            // 
+            this.newCarpetingCostToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newHardwoodCostToolStripMenuItem,
+            this.newCarpetingCostToolStripMenuItem1,
+            this.closeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.newCarpetingCostToolStripMenuItem.Name = "newCarpetingCostToolStripMenuItem";
+            this.newCarpetingCostToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.newCarpetingCostToolStripMenuItem.Text = "File";
+            this.newCarpetingCostToolStripMenuItem.Click += new System.EventHandler(this.newCarpetingCostToolStripMenuItem_Click);
+            // 
+            // newHardwoodCostToolStripMenuItem
+            // 
+            this.newHardwoodCostToolStripMenuItem.Name = "newHardwoodCostToolStripMenuItem";
+            this.newHardwoodCostToolStripMenuItem.Size = new System.Drawing.Size(334, 38);
+            this.newHardwoodCostToolStripMenuItem.Text = "New Hardwood Cost";
+            this.newHardwoodCostToolStripMenuItem.Click += new System.EventHandler(this.newHardwoodCostToolStripMenuItem_Click);
+            // 
+            // newCarpetingCostToolStripMenuItem1
+            // 
+            this.newCarpetingCostToolStripMenuItem1.Name = "newCarpetingCostToolStripMenuItem1";
+            this.newCarpetingCostToolStripMenuItem1.Size = new System.Drawing.Size(334, 38);
+            this.newCarpetingCostToolStripMenuItem1.Text = "New Carpeting Cost";
+            this.newCarpetingCostToolStripMenuItem1.Click += new System.EventHandler(this.newCarpetingCostToolStripMenuItem1_Click);
+            // 
+            // newHardwoodCostToolStripMenuItem1
+            // 
+            this.newHardwoodCostToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cascadeToolStripMenuItem,
+            this.tileVerticallyToolStripMenuItem,
+            this.tileHorizontallyToolStripMenuItem});
+            this.newHardwoodCostToolStripMenuItem1.Name = "newHardwoodCostToolStripMenuItem1";
+            this.newHardwoodCostToolStripMenuItem1.Size = new System.Drawing.Size(114, 38);
+            this.newHardwoodCostToolStripMenuItem1.Text = "Window";
+            this.newHardwoodCostToolStripMenuItem1.Click += new System.EventHandler(this.newHardwoodCostToolStripMenuItem1_Click);
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(288, 38);
+            this.cascadeToolStripMenuItem.Text = "Cascade";
+            this.cascadeToolStripMenuItem.Click += new System.EventHandler(this.cascadeToolStripMenuItem_Click);
+            // 
+            // tileVerticallyToolStripMenuItem
+            // 
+            this.tileVerticallyToolStripMenuItem.Name = "tileVerticallyToolStripMenuItem";
+            this.tileVerticallyToolStripMenuItem.Size = new System.Drawing.Size(288, 38);
+            this.tileVerticallyToolStripMenuItem.Text = "Tile Vertically";
+            this.tileVerticallyToolStripMenuItem.Click += new System.EventHandler(this.tileVerticallyToolStripMenuItem_Click);
+            // 
+            // tileHorizontallyToolStripMenuItem
+            // 
+            this.tileHorizontallyToolStripMenuItem.Name = "tileHorizontallyToolStripMenuItem";
+            this.tileHorizontallyToolStripMenuItem.Size = new System.Drawing.Size(288, 38);
+            this.tileHorizontallyToolStripMenuItem.Text = "Tile Horizontally";
+            this.tileHorizontallyToolStripMenuItem.Click += new System.EventHandler(this.tileHorizontallyToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(334, 38);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(334, 38);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmFlooringCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 1026);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmFlooringCalc";
+            this.Text = "Flooring Calculator";
+            this.Load += new System.EventHandler(this.frmFlooringCalc_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newCarpetingCostToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newHardwoodCostToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newHardwoodCostToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newCarpetingCostToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem cascadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileVerticallyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileHorizontallyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ GallagherSDI/CalculateFlooringCosts/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateFlooringCosts
+{
+    public partial class frmFlooringCalc : Form
+    {
+        public frmFlooringCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFlooringCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newHardwoodCostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form newForm = new frmHardwood();
+            newForm.MdiParent = this;
+            newForm.Show();
+        }
+
+        private void newHardwoodCostToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newCarpetingCostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void newCarpetingCostToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Form newForm = new frmCarpeting();
+            newForm.MdiParent = this;
+            newForm.Show();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileVerticallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void tileHorizontallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form activeForm = this.ActiveMdiChild;
+            if (activeForm != null)
+                activeForm.Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
